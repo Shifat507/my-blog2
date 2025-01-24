8ed8ea54ea1f290f515caa515e336719ef5f4786
Shifat507 <ferdoushasanshifat@gmail.com>
2025-01-24 20:09:35 +0600

    updated home page

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
                         <li><Link href="/blogs">All Blogs</Link></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Blogs</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 ">
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,14 +2,18 @@ import Link from "next/link";
 
 
 const Home = () => {
-    
-    return (
-        <div className="w-11/12 mx-auto">
-            <h1 className="text-3xl font-bold">Your Daily Dose of Knowledge</h1>
 
-            <p>Dive into a world of fresh perspectives, trending topics, and expert opinions. Explore, learn, and stay inspired with our curated collection of engaging blog posts.</p>
+    return (
+        <div className="w-11/12 mx-auto text-center mt-20">
+            <div>
+                <h2 className="text-xl font-bold text-orange-400">-- A blog site portal --</h2>
+                <h1 className="text-3xl font-bold">Your Daily Dose of Knowledge</h1>
+                
             
-            <Link href='/blogs' className="btn btn-primary">See Blogs</Link>
+                <p className="mt-4">Dive into a world of fresh perspectives and endless possibilities. Our blog is your go-to destination for trending topics, expert opinions, and thought-provoking insights. Whether you're looking to explore the latest in technology, lifestyle, business, or personal growth, we've got you covered. Each post is carefully curated to keep you informed, inspired, and ready to take on the world with new ideas. Stay curious, stay engaged, and let our content be your daily source of inspiration and knowledge.</p>
+
+                <Link href='/blogs' className="btn btn-primary mt-16">See Blogs</Link>
+            </div>
         </div>
     );
 };
