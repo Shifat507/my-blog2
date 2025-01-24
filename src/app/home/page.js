cc6ad51c2e0791a35ec472795a8c2337d972314b
Shifat507 <ferdoushasanshifat@gmail.com>
2025-01-24 19:58:55 +0600

    update home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,9 +5,9 @@ const Home = () => {
     
     return (
         <div className="w-11/12 mx-auto">
-            <h1 className="text-3xl font-bold">This is home page.</h1>
+            <h1 className="text-3xl font-bold">Your Daily Dose of Knowledge</h1>
 
-            <p>To see all blogs --</p>
+            <p>Dive into a world of fresh perspectives, trending topics, and expert opinions. Explore, learn, and stay inspired with our curated collection of engaging blog posts.</p>
             
             <Link href='/blogs' className="btn btn-primary">See Blogs</Link>
         </div>
